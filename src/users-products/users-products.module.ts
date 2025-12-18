e45a6749c6057140a1d9864947913547b9eea034
Félix Mielcarek <felix.mielcarek10@gmail.com>
2025-12-18 13:22:58 +0100

    Remove useless DI

--- src/users-products/users-products.module.ts
+++ src/users-products/users-products.module.ts
@@ -3,7 +3,6 @@ import { UsersProductsController } from './controller/users-products.controller'
 import { UsersProductsService } from './service/users-products.service';
 import { UserProduct } from './entities/user-product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from 'src/products/entities/product.entity';
 import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from 'src/products/products.module';
 
@@ -12,7 +11,6 @@ import { ProductsModule } from 'src/products/products.module';
         UsersModule,
         ProductsModule,
         TypeOrmModule.forFeature([UserProduct]),
-        TypeOrmModule.forFeature([Product]),
     ],
     controllers: [UsersProductsController],
     providers: [UsersProductsService],
